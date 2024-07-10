a3b6c03004e88a1cbe43c0bb890bc5d6bfe8b68e
Reham Galal <Reham20496@feng.bu.edu.eg>
2024-07-10 16:30:06 +0300

    DAL

---
+++ MVCProject,DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProject_DAL.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          => optionsBuilder.UseSqlServer("Server = .; Database = MVCProject; Trusted_Connection = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ MVCProject,DAL/Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVCProject_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProject_DAL.Data.Configurations
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10 ,10);
+            builder.Property(D =>D.Code).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(D => D.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+
+
+        }
+    }
+}
